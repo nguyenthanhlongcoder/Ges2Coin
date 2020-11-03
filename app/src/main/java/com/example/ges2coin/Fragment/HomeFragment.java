50b2214977a1ee50c3a5630246ad352a6fe77385
= <=>
2020-11-03 16:39:31 +0700

    add Support, Update Home

--- app/src/main/java/com/example/ges2coin/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/ges2coin/Fragment/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.example.ges2coin.Fragment;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
@@ -11,6 +15,8 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.ges2coin.Adapter.GetMoneyListAdapter;
@@ -23,11 +29,16 @@ public class HomeFragment extends Fragment {
 
     ListView list_getMoney;
     ArrayList<GetMoneyData> data = new ArrayList<>();
+    ImageView img_arrow_home;
+    CardView cardview_home;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        img_arrow_home = view.findViewById(R.id.img_arrow_home);
+        cardview_home = view.findViewById(R.id.cardview_home);
+
 
         data.add(new GetMoneyData("Hoàng Nêu", "1.000.000", 20));
         data.add(new GetMoneyData("Hoàng Nêu", "1.000.000", 20));
@@ -35,6 +46,25 @@ public class HomeFragment extends Fragment {
         list_getMoney = view.findViewById(R.id.list_getMoney);
         GetMoneyListAdapter adapter = new GetMoneyListAdapter(getContext(), R.layout.getmoney_list, data);
         list_getMoney.setAdapter(adapter);
+
+        img_arrow_home.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                if (list_getMoney.getVisibility() == View.GONE){
+                    TransitionManager.beginDelayedTransition(cardview_home, new AutoTransition());
+                    img_arrow_home.setImageResource(R.drawable.ic_baseline_keyboard_arrow_down_24);
+                    list_getMoney.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    TransitionManager.beginDelayedTransition(cardview_home, new AutoTransition());
+                    img_arrow_home.setImageResource(R.drawable.ic_baseline_keyboard_arrow_right_white_24);
+                    list_getMoney.setVisibility(View.GONE);
+                }
+            }
+        });
+
         return view;
     }
 }
--- app/src/main/java/com/example/ges2coin/Fragment/SupportFragment.java
+++ app/src/main/java/com/example/ges2coin/Fragment/SupportFragment.java
@@ -1,21 +1,30 @@
 package com.example.ges2coin.Fragment;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import com.example.ges2coin.R;
 import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 
 public class SupportFragment extends Fragment {
+    ImageView arrow1,arrow2,arrow3,arrow4,arrow5;
     LinearLayout layout_content1, layout_content2,layout_content3,layout_content4,layout_content5;
-    ExpandableRelativeLayout support_content1,support_content2,support_content3,support_content4,support_content5;
+    TextView support_content1,support_content2,support_content3,support_content4,support_content5;
+    CardView cardview_expandable1,cardview_expandable2,cardview_expandable3,cardview_expandable4,cardview_expandable5;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -27,13 +36,115 @@ public class SupportFragment extends Fragment {
         layout_content4 = view.findViewById(R.id.layout_content4);
         layout_content5 = view.findViewById(R.id.layout_content5);
 
+        arrow1 = view.findViewById(R.id.arrow1);
+        arrow2 = view.findViewById(R.id.arrow2);
+        arrow3 = view.findViewById(R.id.arrow3);
+        arrow4 = view.findViewById(R.id.arrow4);
+        arrow5 = view.findViewById(R.id.arrow5);
+
+        cardview_expandable1 = view.findViewById(R.id.cardview_expandable1);
+        cardview_expandable2 = view.findViewById(R.id.cardview_expandable2);
+        cardview_expandable3 = view.findViewById(R.id.cardview_expandable3);
+        cardview_expandable4 = view.findViewById(R.id.cardview_expandable4);
+        cardview_expandable5 = view.findViewById(R.id.cardview_expandable5);
+
+        support_content1  = view.findViewById(R.id.support_content1);
+        support_content2 = view.findViewById(R.id.support_content2);
+        support_content3 = view.findViewById(R.id.support_content3);
+        support_content4 = view.findViewById(R.id.support_content4);
+        support_content5 = view.findViewById(R.id.support_content5);
         layout_content1.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
             @Override
             public void onClick(View v) {
-                support_content1  = view.findViewById(R.id.support_content1);
-                support_content1.toggle();
+
+                if (support_content1.getVisibility() == View.GONE){
+                    TransitionManager.beginDelayedTransition(cardview_expandable1, new AutoTransition());
+                    arrow1.setImageResource(R.drawable.ic_baseline_arrow_gray_down_24);
+                    support_content1.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    TransitionManager.beginDelayedTransition(cardview_expandable1, new AutoTransition());
+                    arrow1.setImageResource(R.drawable.ic_baseline_keyboard_arrow_right_24);
+                    support_content1.setVisibility(View.GONE);
+                }
+
+            }
+        });
+        layout_content2.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+
+                if (support_content2.getVisibility() == View.GONE){
+                    TransitionManager.beginDelayedTransition(cardview_expandable2, new AutoTransition());
+                    arrow2.setImageResource(R.drawable.ic_baseline_arrow_gray_down_24);
+                    support_content2.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    TransitionManager.beginDelayedTransition(cardview_expandable2, new AutoTransition());
+                    arrow2.setImageResource(R.drawable.ic_baseline_keyboard_arrow_right_24);
+                    support_content2.setVisibility(View.GONE);
+                }
+            }
+        });
+        layout_content3.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+
+                if (support_content3.getVisibility() == View.GONE){
+                    TransitionManager.beginDelayedTransition(cardview_expandable3, new AutoTransition());
+                    arrow3.setImageResource(R.drawable.ic_baseline_arrow_gray_down_24);
+                    support_content3.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    TransitionManager.beginDelayedTransition(cardview_expandable3, new AutoTransition());
+                    arrow3.setImageResource(R.drawable.ic_baseline_keyboard_arrow_right_24);
+                    support_content3.setVisibility(View.GONE);
+                }
+            }
+        });
+        layout_content4.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+
+                if (support_content4.getVisibility() == View.GONE){
+                    TransitionManager.beginDelayedTransition(cardview_expandable4, new AutoTransition());
+                    arrow4.setImageResource(R.drawable.ic_baseline_arrow_gray_down_24);
+                    support_content4.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    TransitionManager.beginDelayedTransition(cardview_expandable3, new AutoTransition());
+                    arrow4.setImageResource(R.drawable.ic_baseline_keyboard_arrow_right_24);
+                    support_content4.setVisibility(View.GONE);
+                }
+            }
+        });
+        layout_content5.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+
+                if (support_content5.getVisibility() == View.GONE){
+                    TransitionManager.beginDelayedTransition(cardview_expandable5, new AutoTransition());
+                    arrow5.setImageResource(R.drawable.ic_baseline_arrow_gray_down_24);
+                    support_content5.setVisibility(View.VISIBLE);
+                }
+                else
+                {
+                    TransitionManager.beginDelayedTransition(cardview_expandable5, new AutoTransition());
+                    arrow5.setImageResource(R.drawable.ic_baseline_keyboard_arrow_right_24);
+                    support_content5.setVisibility(View.GONE);
+                }
             }
         });
         return view;
     }
+
 }
